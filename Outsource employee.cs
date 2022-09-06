2205cee99d9247a8be824213d7475eb9e9e4fd3a
Lucas <lucasgnclvs1@gmail.com>
2022-09-06 15:59:28 -0300

    Inheritance - Method overlay

---
+++ Outsource employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Entities
+{
+    internal class Outsource_employee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public Outsource_employee()
+        {
+        }
+
+        public Outsource_employee(string name, int hours, double valueperhour, double additionalCharge) : base(name, hours, valueperhour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+        }
+    }
+}
